f0f68bfc1dba7bf78c87e508765e57810552068f
Adarsh <ad.yadav3@gmail.com>
2021-04-12 22:48:36 +0530

    Link added

--- src/components/mainbutton.jsx
+++ src/components/mainbutton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const MainButton = (props) => {
     return ( 
         <div className="btnmainclass">
-             { props.mainbtnArry.map((elBtn,i) => <button key={i} className="btn" ><a  href={`http://localhost:3000/${elBtn.link}`}>{elBtn.text}</a></button>)}
+             { props.mainbtnArry.map((elBtn,i) => <button key={i} className="btn" ><Link  to={`/${elBtn.link}`}>{elBtn.text}</Link></button>)}
              </div>
      );
     
